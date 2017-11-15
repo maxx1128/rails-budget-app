5cf725e210aea430091575f35bea1e2458f823f9
Max Antonucci <maxx1128@gmail.com>
2017-11-15 10:10:18 -0500

    Fixed actions for updating and removing expenses

--- app/presenters/categories_presenter.rb
+++ app/presenters/categories_presenter.rb
@@ -19,34 +19,20 @@ class CategoriesPresenter < BasePresenter
     @category.id
   end
 
-  def date_link
-    year = @start_date.strftime("%Y")
-    month = @start_date.strftime("%-m")
-
-    "#{year}/#{month}/#{@category.id}"
-  end
-
-  def month_link
-    year = @start_date.strftime("%Y")
-    month = @start_date.strftime("%-m")
-
-    "/categories/#{year}/#{month}/"
-  end
-
   def created_at
     @category.created_at
   end
 
-  def expenses
-    @category.expenses.select { |expense| expense.created_at.between?(@start_date, @end_date) }
+  def budget
+    @category.budget
   end
 
   def total
     category_total(expenses)
   end
 
-  def budget
-    @category.budget
+  def expenses
+    @category.expenses.select { |expense| expense.created_at.between?(@start_date, @end_date) }
   end
 
   def type
@@ -59,6 +45,22 @@ class CategoriesPresenter < BasePresenter
 
   def type_p
     type.pluralize(type)
+  end
+
+
+
+  def date_link
+    year = @start_date.strftime("%Y")
+    month = @start_date.strftime("%-m")
+
+    "#{year}/#{month}/#{@category.id}"
+  end
+
+  def month_link
+    year = @start_date.strftime("%Y")
+    month = @start_date.strftime("%-m")
+
+    "/categories/#{year}/#{month}/"
   end
 
   def balance
