9d41104f71e6f914bfaf0ac4d0b4b9e901ce0bee
Max Antonucci <maxx1128@gmail.com>
2017-11-17 12:38:51 -0500

    Added helper for random money quotes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,6 +23,22 @@ module ApplicationHelper
     yield(presenter) if block_given?
   end
 
+  def random_finance_quote
+    quotes = [
+      "Balancing your money is the key to having enough.",
+      "Every time you borrow money, you're robbing your future self.",
+      "When you work on something that only has the capacity to make you 5 dollars, it does not matter how much harder you work – the most you will make is 5 dollars.",
+      "Money, like emotions, is something you must control to keep your life on the right track.",
+      "There is only one way to make money: sell something. You're either selling your time or a product. The secret? Productize your time. That's freedom.",
+      "Money per se does not make you happy - it's what it enables you to do with your life.",
+      "Chase after money and the stuggering race will never end; but reach out for successful ideas and the sources of money will chase after you to fill your pockets.",
+      "Money does not dictate your lifestyle, it's what you do to get it and how you manage your finances that determines your lifestyle.",
+      "Budgeting has only one rule: Do not go over budget."
+    ]
+
+    "\"#{quotes.sample}\""
+  end
+
   def h
     ApplicationController.helpers
   end
